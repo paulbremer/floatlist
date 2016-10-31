a7c3285c517f0cd331c1a1ed803f969df114dc1c
Paul Bremer <info@paulbremer.nl>
2016-10-31 12:56:16 -0500

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 5000;
 
